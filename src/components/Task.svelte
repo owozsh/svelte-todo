58e58bd88ac686a0d4352d357b6756b26537b112
owozsh <owozsh@posteo.net>
2021-08-15 20:05:17 -0300

    add transition to task component

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -17,16 +17,23 @@
         padding: 20px 20px;
         border-radius: 10px;
 
-        //font-family: 'STIX Two Text', serif;
         font-size: 1.5rem;
+
+        cursor: pointer;
+
+        transition-duration: 0.2s;
+        transition-timing-function: cubic-bezier();
+
+        &:hover {
+            transform: translate(0, -3px);
+            background-color: $bright_white;
+        }
     }
 
     .taskToggle {
         width: 1.5rem;
         height: 1.5rem;
         margin-right: 1rem;
-
-        background-color: $white;
 
         border-radius: 50%;
         border: 2px solid $black;

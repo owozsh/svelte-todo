3d8b2609886e4e8af393d9c881f7d483fc0f3ab9
owozsh <owozsh@posteo.net>
2021-08-15 10:06:32 -0300

    add Sidebar

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Sidebar from "./components/Sidebar.svelte";
+</script>
+
+<main>
+	<Sidebar/>
+</main>
+
+<style lang="scss">
+	:global(*) {
+		box-sizing: border-box;
+	}
+
+	:global(body) {
+		height: 100vh;
+		background-color: #dddddd;
+		
+		margin: 0;
+		padding: 0;
+	}
+
+	main {
+		display: flex;
+		height: 100%;
+	}
+</style>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,43 @@
+<nav class="sidebar">
+    <ul>
+        <li>Today</li>
+        <li>Everyday</li>
+        <li>Someday</li>
+    </ul>
+</nav>
+
+<style lang="scss">
+    :global(ul) {
+        border: 0;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :global(li) {
+        border: 0;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        display: inline-block;
+    }
+
+    nav {
+        display: flex;
+
+        flex-direction: column;
+        align-items: center;
+
+
+        height: 100%;
+        width: 300px;
+        
+        background-color: #1f1f1f;
+    }
+
+    li {
+        color: #FFF;
+        list-style: none;
+    }
+</style>

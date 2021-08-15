a2a138b8ace807349ce8caa8b5d70ed7baba39bd
owozsh <owozsh@posteo.net>
2021-08-15 10:25:05 -0300

    change Sidebar layout; add MainView.svelte

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -8,32 +8,47 @@
 
 <style lang="scss">
     :global(ul) {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+
+        font-size: 36px;
+
         border: 0;
         margin: 0;
-        padding: 0;
+        padding: 50px;
         box-sizing: border-box;
+        background-color: #1f1f1f;
     }
 
     :global(li) {
         border: 0;
-        margin: 0;
         padding: 0;
         box-sizing: border-box;
 
+        margin-bottom: 50px;
+        
+        &:last-of-type {
+            margin-bottom: 0;
+        }
+
         display: inline-block;
+
+        cursor: pointer;
     }
 
     nav {
         display: flex;
 
         flex-direction: column;
-        align-items: center;
+        justify-content: center;
+        align-items: flex-end;
 
+        padding-right: 70px;
 
         height: 100%;
-        width: 300px;
+        width: 30%;
         
-        background-color: #1f1f1f;
     }
 
     li {

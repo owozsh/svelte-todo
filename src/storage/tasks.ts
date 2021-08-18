c21c1a17b1d6ad7bc073f9e81c8dd12f261aea26
owozsh <owozsh@posteo.net>
2021-08-18 11:50:10 -0300

    Implement localStorage

--- src/storage/tasks.ts
+++ src/storage/tasks.ts
@@ -6,16 +6,27 @@ type Task = {
     status: boolean;
 }
 
-export const tasks = writable([]);
+export const tasks = writable(JSON.parse(localStorage.getItem('tasks')) || []);
+console.log(JSON.parse(localStorage.getItem('tasks')));
+
+//export const tasks = writable([]);
 
 export function addTask(title) {
-    const newId = tasks.subscribe(tasks => tasks.length);
-    tasks.update(tasks => tasks = [...tasks, {id: newId, title: title, status: false}]);
+    tasks.update(tasks => {
+        const newId = tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1;
+        return [...tasks, {id: newId, title: title, status: false}];
+    });
+    tasks.subscribe(tasks => localStorage.setItem('tasks', JSON.stringify(tasks)));
+
+    console.log(JSON.parse(localStorage.getItem('tasks')));
 }
 
 export function deleteTask(id) {
     setTimeout(
         () => tasks.update(tasks => tasks = tasks.filter(task => task.id !== id)),
         1000
-    )
+    );
+
+    tasks.subscribe(tasks => localStorage.setItem('tasks', JSON.stringify(tasks)));
+    console.log(JSON.parse(localStorage.getItem('tasks')));
 }
